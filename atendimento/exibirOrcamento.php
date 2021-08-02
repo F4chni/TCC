46a6d4dff0bfd0605e1bb14c5d7b08f40e993671
PORRADENOME <84040149+PORRADENOME@users.noreply.github.com>
2021-08-02 16:55:24 -0300

    removação

--- atendimento/exibirOrcamento.php
+++
@@ -1,7 +0,0 @@
-<?php
-  //  require "../seguranca.php";
-
-    include("../configurações/bootstrap.php");
-    include("../configurações/menu.php");
-    require "../configurações/segurança.php";
-    include 'montaOrcamento.php';
